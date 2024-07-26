d23f6ba3fc4a3a3161c89da365f44ae9f53ff103
Assil <astradhdbjd@gmail.com>
2024-07-26 20:54:17 +0100

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:device_preview/device_preview.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    DevicePreview(
+      enabled: true,
+      builder: (context) => const BarberApp(),
+    ),
+  );
+}
+
+class BarberApp extends StatelessWidget {
+  const BarberApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(),
+    );
+  }
+}
