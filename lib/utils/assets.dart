36d60a837a0a18784f6fb32692599be9d4bb59ea
Assil <astradhdbjd@gmail.com>
2024-07-26 21:13:59 +0100

    add assets & onboarding view

---
+++ lib/utils/assets.dart
@@ -0,0 +1,40 @@
+class Assets {
+  Assets._();
+  
+  /// Assets for imagesBarber
+  /// assets/images/barber.png
+  static const String imagesBarber = "assets/images/barber.png";
+
+  /// Assets for imagesBeard
+  /// assets/images/beard.png
+  static const String imagesBeard = "assets/images/beard.png";
+
+  /// Assets for imagesBoy
+  /// assets/images/boy.jpg
+  static const String imagesBoy = "assets/images/boy.jpg";
+
+  /// Assets for imagesCutting
+  /// assets/images/cutting.png
+  static const String imagesCutting = "assets/images/cutting.png";
+
+  /// Assets for imagesDiscount
+  /// assets/images/discount.png
+  static const String imagesDiscount = "assets/images/discount.png";
+
+  /// Assets for imagesFacials
+  /// assets/images/facials.png
+  static const String imagesFacials = "assets/images/facials.png";
+
+  /// Assets for imagesHair
+  /// assets/images/hair.png
+  static const String imagesHair = "assets/images/hair.png";
+
+  /// Assets for imagesKids
+  /// assets/images/kids.png
+  static const String imagesKids = "assets/images/kids.png";
+
+  /// Assets for imagesShaving
+  /// assets/images/shaving.png
+  static const String imagesShaving = "assets/images/shaving.png";
+}
+
